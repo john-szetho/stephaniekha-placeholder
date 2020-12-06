9d2306dd1745d73f62a994ae358e7cc1774d9910
John Szetho <jszetho@gmail.com>
2020-12-06 15:49:50 +1100

    placeholder initialise

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('.contact-links-toggle').addEventListener('click', function() {
+
+document.querySelector('.deets').classList.toggle('hide')
+document.querySelector('.contact-links-toggle').classList.toggle('active')
+
+return false
+    
+})
